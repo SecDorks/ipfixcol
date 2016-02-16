d88a34cbb277254c985702018a6f31c7edf39d3a
Kirc <kirc@secdorks.net>
2016-02-16 08:20:34 +0100

    HTTPFieldMerge: fixed byte order conversion bug in Cisco processor

--- plugins/intermediate/httpfieldmerge/vendor_proc/cisco.c
+++ plugins/intermediate/httpfieldmerge/vendor_proc/cisco.c
@@ -273,7 +273,7 @@ void cisco_data_rec_processor(uint8_t *rec, int rec_len, struct ipfix_template *
         if (new_field_len >= 255) {
             /* Set new (variable) length in byte two and three of field. Note that
              * 'field_offset' contains the offset to the actual data, so the field
-             * length is stored at 'field_offset - 2'.
+             * length is stored at 'field_offset - 2'
              */
             new_field_len = htons(new_field_len);
             memcpy(rec + field_offset - 2, &new_field_len, sizeof(new_field_len));
@@ -283,12 +283,14 @@ void cisco_data_rec_processor(uint8_t *rec, int rec_len, struct ipfix_template *
              */
             memmove(rec + field_offset - 2, rec + field_offset, new_field_len);
             field_offset -= 2;
+            new_field_len = htons(new_field_len);
             memset(rec + field_offset - 1, new_field_len, 1);
         } else {
             /* Set new (variable) length in first byte of field. Note that 'field_offset'
              * contains the offset to the actual data, so the field length is stored at
              * 'field_offset - 1'
              */
+            new_field_len = htons(new_field_len);
             memset(rec + field_offset - 1, new_field_len, 1);
         }
 
